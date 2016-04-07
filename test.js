0440cee4ee89c08336519f14d9b6bd2fd5676700
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:18:33 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ import pify from 'pify';
 
 test('main', async t => {
 	const stdout = await pify(childProcess.execFile)('./cli.js', ['fixture.json']);
-	t.same(JSON.parse(stdout), {unicorn: 'cake'});
+	t.deepEqual(JSON.parse(stdout), {unicorn: 'cake'});
 });
 
 test('stdin', async t => {
 	const stdout = await pify(childProcess.exec)('echo \'{/*rainbows*/"unicorn":"cake"}\' | ./cli.js');
-	t.same(JSON.parse(stdout), {unicorn: 'cake'});
+	t.deepEqual(JSON.parse(stdout), {unicorn: 'cake'});
 });
